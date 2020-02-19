cc99902f6850294d2a11086e8763b64857042c2c
Joseph Turner <59948452+jturne46@users.noreply.github.com>
2020-02-19 15:35:37 -0600

    Update csvreader.cpp

--- csvreader.cpp
+++ csvreader.cpp
@@ -11,61 +11,76 @@ int main(){
 	std::string fileName = "3bed.csv";
 	int r, c;
 	r = rows(fileName);				// Needed for 'for loop' to iterate over whole file.
-						
-
 	c = cols(fileName);				// What will this be for? 
 	
 	XMLDocument xmlDoc;				// creates XML Doc to return
-        XMLNode * pRoot = xmlDoc.NewElement("Root");	// creates  <root> element
+        XMLNode * pRoot = xmlDoc.NewElement("housing");	// creates  <root> element
         xmlDoc.InsertFirstChild(pRoot);			// inserts as first element
 
         std::string temp;				// holds the string when parsing
         std::string y;					// not sure yet
         std::ifstream file(fileName);			// Brings in CSV as fileBuffer object (in the stream)
 
-        std::getline(file, temp);			// Stores chars into 'temp' until the first '\n' is found
+        std::getline(file, temp, '\n');	
+	//	while(std::getline(file,temp,','))
+	//	{
+	//		std::cout << temp <<"\n";
+	//	}
+				// Stores chars into 'temp' until the first '\n' is found
 
-	XMLElement * root = xmlDoc.NewElement("state"); // creates element <state>
+	XMLElement * root = xmlDoc.NewElement("states"); // creates element <state>
 	pRoot -> InsertEndChild(root);			// inserts <state> into end
 
 	XMLElement * state;				// undeclared XMLElements meant to house the future ones
 	XMLElement * zip;
 	XMLElement * city;
-	XMLElement * price;
 
-	for(int i = 0; i <= r; i++){			// for the total number of r Rows in this CSV file
+
+	//const char * zip_id, city_id, state_id;
+	
+	for(int i = 0; i < r + 1; i++){			// for the total number of r Rows in this CSV file
 
 		
-		std::getline(file, temp);		// Obtain the first row of CSV into a String
+		std::getline(file, temp, '\n');		// Obtain the first row of CSV into a String
 		std::stringstream sstream(temp);	// puts into stringstream for manipulation
 
-        	std::getline(sstream, temp, ',');	// puts RegionID into temp 
+
+        std::getline(sstream, temp, ',');	// puts RegionID into temp 
 		std::getline(sstream, temp, ',');	// puts RegionName (The actual zip code) into temp
-		zip = xmlDoc.NewElement(temp.c_str());	// assigns the XMLElement 'zip' with the char* (or string) value of temp 
-
+		
+		std::string zip_id = temp;
+		zip = xmlDoc.NewElement("zip");
+		zip -> SetAttribute("code", zip_id.c_str());	// assigns the XMLElement 'zip' with the char* (or string) value of temp 
+		
 		std::getline(sstream, temp, ',');	// save city value into the temp string
-		city = xmlDoc.NewElement(temp.c_str()); // assign XMLElement
+		std::string city_id = temp;
+		city = xmlDoc.NewElement("city");
+		city ->SetAttribute("city_name", city_id.c_str()); // assign XMLElement
 
-                std::getline(sstream, temp, ',');	// then the state
-	        state = xmlDoc.NewElement(temp.c_str());
+        std::getline(sstream, temp, ',');	// then the state
+		std::string state_id =temp;
+		state = xmlDoc.NewElement("state");
+		state -> SetAttribute("state_name", state_id.c_str());
+	        
 
-		for(int j = 0; j <= r - 4; j++){
+		for(int j = 0; j < 4; j++){
 			std::getline(sstream, temp, ',');
 		}
 
 		temp = temp.substr(0, temp.length() - 1);
-
-		price = xmlDoc.NewElement("Price");	
-		price -> SetText(temp.c_str());		// close them up by inserting them in layers
-       	 	root  -> InsertEndChild(state);		// If SetText in any upper elements, it fucks up the format
+		
+		XMLElement * price = xmlDoc.NewElement("Price");	
+		
+       	root  -> InsertEndChild(state);		// If SetText in any upper elements, it fucks up the format
 		state -> InsertEndChild(city);
 		city  -> InsertEndChild(zip);
 		zip -> InsertEndChild(price);
+		price -> SetText(temp.c_str());		// close them up by inserting them in layers
 		sstream.str(std::string());
 
 	}
 
-
+	
 
 	xmlDoc.SaveFile("yolo.xml");
 
